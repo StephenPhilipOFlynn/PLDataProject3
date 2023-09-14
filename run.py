969d6e9bd02dcb014bc6cdf552411619070deead
Stephen OFlynn <soflynn@tcd.ie>
2023-09-14 17:08:35 +0000

    correct Pep8 hastag format

--- run.py
+++ run.py
@@ -19,25 +19,25 @@ SHEET = GSPREAD_CLIENT.open('Premier League Data')
 datasetone = SHEET.worksheet('Sheet1')
 
 data = datasetone.get_all_values()
-#check data type
+# check data type
 data_type = type(data)
-#data is a list, turn into dataframe
+# data is a list, turn into dataframe
 df = pd.DataFrame(data)
-#skip row 1 with includes spreadsheet titles
+# skip row 1 with includes spreadsheet titles
 df = df.iloc[1:]
-#rename columns to access data correctly in dataframe sheet 1
+# rename columns to access data correctly in dataframe sheet 1
 df.columns = ['Div', 'Date', 'H_Team', 'A_Team', 'FT_H_Gls', 'FT_A_Gls', 
 'FT_Result', 'HT_H_Gls', 'HT_A_Gls', 'HT_Result', 'Ref', 'H_Shts', 'A_Shts', 'H_Shts_Trgt', 
 'A_Shts_Trgt', 'H_Fouls', 'A_Fouls', 'H_Corners', 'A_Corners', 'H_Yellow', 'A_Yellow', 'H_Red', 'A_Red']
 
-#check second dataset accessible in terminal
+# check second dataset accessible in terminal
 datasettwo = SHEET.worksheet('Sheet2')
 data_two = datasettwo.get_all_values()
-#The second data set is also a list. Turn into a dataframe. 
+# The second data set is also a list. Turn into a dataframe. 
 df_two = pd.DataFrame(data_two)
-#remove first row with titles for data manipulation
+# remove first row with titles for data manipulation
 df_two = df_two.iloc[1:]
-#rename columns in second dataset for dataframe
+# rename columns in second dataset for dataframe
 df_two.columns = ['team', 'category', 'league_pos', 'games_televised', 'fin_tv_revenue', 'matches_played', 
 'games_won', 'games_drawn', 'general_lost', 'goals_scored', 'goals_conceded', 'total_goal_diff', 'total_points', 'squad_size', 'squad_aver_age', 
 'squad_overseas', 'fin_team_market', 'fin_market_average', 'total_passes',
@@ -46,66 +46,66 @@ df_two.columns = ['team', 'category', 'league_pos', 'games_televised', 'fin_tv_r
 'att_goals_counter', 'att_goals_freekick', 'def_saves', 'def_blocks', 'def_intercept', 'def_tackles', 'def_tackles_last_man',
 'def_clearances', 'def_headed_clearances', 'def_penalty_conceded', 'att_poss', 'pass_acc']
 
-#Variables for Tactical Question 1
-#count referee appearances to begin working out which referee gives out most cards
+# Variables for Tactical Question 1
+# count referee appearances to begin working out which referee gives out most cards
 referee_appearances = df['Ref'].value_counts()
-#convert from concatenated strings to integers
+# convert from concatenated strings to integers
 df['H_Red'] = df['H_Red'].astype(int)
 df['A_Red'] = df['A_Red'].astype(int)
-#create new column total reds cards per match
+# create new column total reds cards per match
 df["Ttl_Mtch_Red"] = df[['H_Red', 'A_Red']].sum(axis=1)
-#find which referees gave most red cards
+# find which referees gave most red cards
 ref_red_cards = df.groupby('Ref')['Ttl_Mtch_Red'].sum()
-#find out which referee gives the most cards per game
+# find out which referee gives the most cards per game
 ref_redcards_permatch = ref_red_cards / referee_appearances
 # most to least
 ref_redcards_permatch_desc = ref_redcards_permatch.sort_values(ascending=False)
 topfour_most_red_card_refs = ref_redcards_permatch_desc.head(4)
 
-#Variables for Tactical Question 2
-#work out relationship between match result at half-time, result at full time
-#number of games in a season
+# Variables for Tactical Question 2
+# work out relationship between match result at half-time, result at full time
+# number of games in a season
 total_matches = len(df)
-#number of matches where result is same at full time as it was at half time
+# number of matches where result is same at full time as it was at half time
 same_match_result = len(df[df['HT_Result'] == df['FT_Result']])
 same_result_percentage = (same_match_result / total_matches) * 100
-#find the number of games where away team winning at half time wins
+# find the number of games where away team winning at half time wins
 away_team_win_both_halfs = df[(df['HT_Result'] == 'A') & (df['FT_Result'] == 'A')]
 away_team_win_first_half_only = df[(df['HT_Result'] == 'A') & (df['FT_Result'] != 'A')]
 count_away_team_win_both_halves = len(away_team_win_both_halfs)
 count_away_team_win_first_half_only = len(away_team_win_first_half_only)
-#calculate games being won by away team at half, irregardless of second half result - combine
+# calculate games being won by away team at half, irregardless of second half result - combine
 away_wins_first_half = count_away_team_win_first_half_only + count_away_team_win_both_halves
-#calculate percentage of away team conversions
+# calculate percentage of away team conversions
 second_half_away_conversion_rate = (count_away_team_win_both_halves / away_wins_first_half) * 100
-#SAME ANALYSIS ON HOME TEAM
-#find the number of games where home team winning at half time wins
+# Same analysis on home team performances
+# find the number of games where home team winning at half time wins
 home_team_win_both_halfs = df[(df['HT_Result'] == 'H') & (df['FT_Result'] == 'H')]
 home_team_win_first_half_only = df[(df['HT_Result'] == 'H') & (df['FT_Result'] != 'H')]
 count_home_team_win_both_halves = len(home_team_win_both_halfs)
 count_home_team_win_first_half_only = len(home_team_win_first_half_only)
-#calculate games being won by home team at half, irregardless of second half result and combine results
+# calculate games being won by home team at half, irregardless of second half result and combine results
 home_wins_first_half = count_home_team_win_first_half_only + count_home_team_win_both_halves
-#calculate percentage of home team conversions
+# calculate percentage of home team conversions
 second_half_home_conversion_rate = (count_home_team_win_both_halves / home_wins_first_half) * 100
 
 # Variables for Tactical Question 3
-#remove commmas in the two columsn to perform mathematical operations
+# remove commmas in the two columsn to perform mathematical operations
 df_two['total_passes'] = df_two['total_passes'].str.replace(',', '')
 df_two['total_passes_long'] = df_two['total_passes_long'].str.replace(',', '')
-#consider what teams most reliant on long passes. we will compare long passes to total passes for this purpose
-#first convert values to integers
+# consider what teams most reliant on long passes. we will compare long passes to total passes for this purpose
+# first convert values to integers
 df_two['total_passes_long'] = df_two['total_passes_long'].astype(int)
 df_two['total_passes'] = df_two['total_passes'].astype(int)
-#divide long passes by total passes for each team
+# divide long passes by total passes for each team
 df_two['ratio_long_passes'] = df_two['total_passes_long'] / df_two['total_passes']
 df_two['perc_long_passes'] = df_two['ratio_long_passes'] * 100
 #create separate dataframe of just team and their percentage of long balls
 style_of_passing = df_two[['team', 'perc_long_passes']].sort_values(by='perc_long_passes', ascending=False)
 
 # Variables for Tactical Question 4
-#first convert to int
-#find out which teams were most reliant on counter attacks as a proportion of their total goals
+# first convert to int
+# find out which teams were most reliant on counter attacks as a proportion of their total goals
 df_two['att_goals_counter'] = df_two['att_goals_counter'].astype(int)
 df_two['goals_scored'] = df_two['goals_scored'].astype(int)
 df_two['counter_attack_goal_perc'] = (df_two['att_goals_counter'] / df_two['goals_scored']) * 100
@@ -143,7 +143,6 @@ def question_4():
     print("The following teams are most reliant on counter attacks to score goals:")
     print(most_counterattacking_teams[['team', 'counter_attack_goal_perc']])
 
-
 def tactical_questions():
     while True:
         print(" ")
@@ -155,7 +154,7 @@ def tactical_questions():
         print("4. Which teams are most reliant on counter attacking goals in their style of play?")
         print("5. Reset")
 
-        choose = input("Enter the number of the question to choose or 5 to quit: (eg. '1')")
+        choose = input("Enter the number of the question to choose or enter 5 to exit: (eg. '1')")
 
         if choose == '1':
             question_1()
@@ -168,7 +167,7 @@ def tactical_questions():
         elif choose == '5':
             print("Thank you and goodbye")
         else:
-            print("Invalid choice. Please enter a number from 1 to 4")
+            print("Invalid choice. Please enter a number from 1 to 5")
 
 
 tactical_questions()
